6ad2188e70680976c1674a65687b4aa140bc2918
Sandrine FAUCHER <sandrine.faucher@maif.fr>
2024-08-21 10:29:41 +0200

    initialisation du projet

---
+++ src/main/java/fr/maif/forknow/ForknowApplication.java
@@ -0,0 +1,13 @@
+package fr.maif.forknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForknowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForknowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/maif/forknow/ForknowApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.maif.forknow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForknowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
